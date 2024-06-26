2c31984403e6d18f9d8933e46c4a5c36f266c995
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-06-26 15:21:29 +0200

    partly update tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log(getRandomHexColor());
+
+const btnChange = document.querySelector("button.change-color");
+const bodyColor = document.querySelector("body");
+const colorNameDisplay = document.querySelector("span.color");
+
+btnChange.addEventListener("click", getRandomHexColor);
+bodyColor.style.backgroundColor = getRandomHexColor;
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector("#controls > button:nth-child(2)"); //create
+const btnCreate2 = document.querySelector('button[data-create]'); //create
+const btnDestroy = document.querySelector("#controls > button:nth-child(3)"); //destroy
+const btnCreate2 = document.querySelector('button[data-destroy]'); //destroy
+
+createBoxes(amount){
+  if (amount>=1 && amount <=100) {
+  div#boxes
+  }
+  const box = document.createElement("div#boxes")
+  document.body.append(box);
+// input.reset();
+}
+
+
+// destroyBoxes(event) {
+
+// }
